d118c5c3353f3285d18fb817897a55ade0343f2b
gmsabbirahmed1 <148498744+gmsabbirahmed1@users.noreply.github.com>
2024-08-29 01:04:01 +0600

    Update be c codent.c

--- be c codent.c
+++ be c codent.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    while(1)
+    {
+        scanf("%d", &x);
+        scanf("%d", &y);
+
+
+        if  ( x>0 && y>0 )
+        {
+            printf("primeiro\n");
+        }
+        else if( x>0 && y<0)
+        {
+            printf("quarto\n");
+        }
+        else if( x<0 && y<0)
+        {
+            printf("terceiro\n");
+        }
+        else if( x<0 && y>0)
+        {
+            printf("segundo\n");
+        }
+
+        else
+        {
+            break;
+        }
+    }
+    return 0;
+}
 
